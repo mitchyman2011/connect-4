9cb14665fca0a52b322fd2bd83146f7f7c90d634
mitchyman2011 <43811416+mitchyman2011@users.noreply.github.com>
2020-02-17 18:06:09 +0000

    Update connetc4.py

--- connetc4.py
+++ connetc4.py
@@ -5,14 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import time
-
+from numba import njit
+@njit
 def row(column,row,game): 
     for load in range(0,n):
         
         if game[load][column]==0:
 
             return load
-        
+    return (-1)   
 def draw(board):
     for x in range(0,k):
         for y in range(0,n):
@@ -34,7 +35,7 @@ def Win(board, player):
                     l.append(y)
                 if len(set(o))==1:
                     
-                    if o[1]== player:
+                    if o[0]== player:
                         
                         
                         return (True,t,l)
@@ -51,7 +52,7 @@ def Win(board, player):
                 if len(set(o))==1:
                     
                     
-                    if o[1]== player:
+                    if o[0]== player:
                         return (True,t,l)
             if y<=(n-connect) and x<=(k-connect):
                 o=[]
@@ -63,49 +64,52 @@ def Win(board, player):
                     l.append(y+i)
                 if len(set(o))==1:
                     
-                    if o[1]== player:
+                    if o[0]== player:
                         return (True,t,l)
             if (connect-1)<=x and y<=(n-connect):
                 o=[]
                 t=[]
                 l=[]
-                print(x)
+                
                 for i in range(0,connect):
                     o.append(board[y+i][x-i]) 
                     t.append(x-i)
                     l.append(y+i)
                 if len(set(o))==1:
                     
-                    if o[1]== player:
+                    if o[0]== player:
                         
                         
                         return (True,t,l)
-    return False,0,0      
+    return False,0,0
+@njit      
 def win_pos(board, player):
     # check horizontal spaces
     for y in range(n):
         for x in range(k):
-            if x<=(k-4):
+            if x<=(k-connect):
                 if board[y][x] == player and board[y][x+1] == player and board[y][x+2] == player and board[y][x+3] == player:
                     return True
 
-            if y<=(n-4):
+            if y<=(n-connect):
                 if board[y][x] == player and board[y+1][x] == player and board[y+2][x] == player and board[y+3][x] == player:
                     
                     return True
                     
-            if y<=(n-4) and x<=(k-3):
+            if y<=(n-connect) and x<=(k-connect):
                 if board[y][x] == player and board[y+1][x+1] == player and board[y+2][x+2] == player and board[y+3][x+3] == player:
+                    print(player,)
                     return True 
 
    # check / diagonal spaces
     for x in range(k):
-        for y in range(n-3):
+        for y in range(n-connect):
             if board[y][x] == player and board[y+1][x-1] == player and board[y+2][x-2] == player and board[y+3][x-3] == player:
                 return True
-    for x in range(k-3 ):
+    for x in range(k-connect ):
         for y in range(n):
             if board[y][x] == player and board[y-1][x+1] == player and board[y-2][x+2] == player and board[y-3][x+3] == player:
+                print("im going thhrough here")
                 return True
     return False
 def col():
@@ -125,9 +129,11 @@ def plank(game_b,rows,player):
     if rows == None:
         return (game_b,column_choice,"m")    
     game_b=gab(game_b,player,rows,column_choice)
-    return(game_b,column_choice,player)       
+    return(game_b,column_choice,player)   
+@njit    
 def terminal(board):
     return win_pos(board,2) or win_pos(board,1) 
+@njit
 def minimax(board, max_play, depth ,alpha,beta,rot,player,oplayer):
     if depth==0 or terminal(board):
         
@@ -136,7 +142,7 @@ def minimax(board, max_play, depth ,alpha,beta,rot,player,oplayer):
                 return(None, 100000)
                 
             if win_pos(board, oplayer):
-                return(None, -100000)
+                return(None, -10000000)
             else:
                 print("oh know")
                 return (None,0)
@@ -148,16 +154,16 @@ def minimax(board, max_play, depth ,alpha,beta,rot,player,oplayer):
         column=random.randint(0,k-1)
         for col in range(0,k):
             
-            try:
-                rows =row(col,rot,board)
-                if rows == None:
-                    break
-                b_copy=board.copy()
-                b_copy=game_board(b_copy,1,rows,col)
-                b,new_score=minimax(b_copy,False,depth-1,alpha,beta,rot,player,oplayer)
+            #try:
+            rows =row(col,rot,board)
+            if rows == -1:
+                break
+            b_copy=board.copy()
+            b_copy=game_board(b_copy,1,rows,col)
+            b,new_score=minimax(b_copy,False,depth-1,alpha,beta,rot,player,oplayer)
                 
-            except:
-                new_score=-math.inf
+            #except:
+                
                 
             
             if new_score > value:
@@ -177,19 +183,14 @@ def minimax(board, max_play, depth ,alpha,beta,rot,player,oplayer):
         column=random.randint(0,k-1)
         for col in range(0,k):
             
-            try:
-                rows =row(col,rot,board)
-                if rows == None:
-                    break
-                b_copy=board.copy()
-                b_copy=game_board(b_copy,2,rows,col)
-                b,new_score=minimax(b_copy,True,depth-1,alpha,beta,rot,player,oplayer)
+            #try:
+            rows =row(col,rot,board)
+            if rows == -1:
+                break
+            b_copy=board.copy()
+            b_copy=game_board(b_copy,2,rows,col)
+            b,new_score=minimax(b_copy,True,depth-1,alpha,beta,rot,player,oplayer)
                 
-                
-            except:
-                
-                new_score=math.inf
-                pass
             if new_score< value:
                 value=new_score
                 column=col
@@ -201,7 +202,7 @@ def minimax(board, max_play, depth ,alpha,beta,rot,player,oplayer):
                break
                 
         return column, value
-
+@njit
 def score_pos(board,player,op):
    
     
@@ -290,6 +291,7 @@ def score_pos(board,player,op):
                 score=score-10
     
     return score
+@njit
 def game_board(game_map, player, row, column):
     
     game_map[row][column] = player
@@ -307,7 +309,7 @@ def gab(game_map, player, row, column):
     #print(game_map)
     return(game_map)
 def plot(game_map,t,y,l):
-    print("im here")
+    
     for i in range(0,n):
         for j in range(0,k):
             for a in  range(0,total_players+1):
@@ -337,10 +339,13 @@ def play():
             play()
         try:
            global h_p_c 
-           h_p_c=int(input("how many humans(selecting one will be against the AI)"))
+           h_p_c=int(input("how many humans(selecting one will be against the AI must be less than 7)"))
            global connect
            connect=int(input("how many connects"))
         except:
+            print("Mitchell is a big penis")
+            play()
+        if connect<=1 or 7<h_p_c:
             print("OMG humans anoy me!")
             play()
         global total_players
@@ -371,7 +376,7 @@ def play():
         hellow = input("you ready bro")
         print("i dont care what you think your playin")
         x=1
-        i=0
+        i=1
         count=0
         if h_p_c<=1:
                
@@ -398,14 +403,15 @@ def play():
                    depth=9
                if difficalty==5:
                    depth=11
-        
+                  
+        plot(game,0,0,"Let's Start Dude") 
         while x==1:
             rot=n-1
            
             if h_p_c<2:
                 
                 
-                plot(game,0,0,"Let's Start Dude")
+                
                 if i<1:
                     current_player="m"
                     while  current_player=="m":
@@ -422,11 +428,12 @@ def play():
                 else:
                     current_player=1
                     oplayer=2
-                    time_start = time.time()
                     rows="m"
                     while rows=="m":
+                        time_b=time.time()
+                        print(depth)
                         column,maxscore=minimax(game, True, depth,-math.inf,math.inf,rot,current_player,oplayer)
-                        print("hi")
+                        
                         chair=random.randint(0,40)
                         if chair==1:
                             print("EZ PZ!!!")
@@ -442,7 +449,8 @@ def play():
                         rows=row(column,rot,game)
                         
                         game=gab(game,current_player,rows,column)
-                        time_elapsed = (time.clock() - time_start)
+            
+                        time_elapsed = time.time() - time_b
                         print(time_elapsed)
                         i=0
                         count=count+1
@@ -464,17 +472,14 @@ def play():
             else:
             
                 
-                for l in range(1,h_p_c+1):
-                    colour_player=colour[l]
-                    plt.plot(l,0,colour_player,markersize=10)
-                plt.title("The player colours on the x axis")
-                plt.show()
+            
                 
-                print("yo")
-                plot(game,0,0,"Let's Start Dude")
+               
                 for p in range(1,h_p_c+1):
                     current_player = p
                     game,column_choice,current_player=plank(game,rot,current_player)
+                    if current_player=="m":
+                        print("you miss your go")
                     true,t,y=Win(game,current_player)    
                         
                     if true== True:
